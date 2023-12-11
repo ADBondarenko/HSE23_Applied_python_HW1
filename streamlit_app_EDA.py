cc5f5b0ce79ba8d7eab81eab02bf64ba112a2424
Artem Bondarenko <Regall.B@yandex.ru>
2023-12-11 21:58:49 +0300

    Changed some code

--- streamlit_app_EDA.py
+++ streamlit_app_EDA.py
@@ -1,3 +1,12 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np
+import os
+
+conn = st.connection("postgresql", type="sql")
+df = conn.query('SELECT * FROM D_merged_processed limit 10;', ttl="10m")
+
+for row in df.itertuples():
+    st.write(f"{row.id} has a :{row.loan_id}:")
+    
+st.dataframe(df, use_container_width=True)
