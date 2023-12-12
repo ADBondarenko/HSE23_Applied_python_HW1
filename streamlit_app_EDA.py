2d1c3437e0ce613ad9d42b12aa70b1c643b4980f
Artem Bondarenko <Regall.B@yandex.ru>
2023-12-12 12:06:37 +0300

    Update streamlit_app_EDA.py

--- streamlit_app_EDA.py
+++ streamlit_app_EDA.py
@@ -4,9 +4,20 @@ import numpy as np
 import os
 
 conn = st.connection("postgresql", type="sql")
-df = conn.query('SELECT * FROM D_merged_processed limit 10;', ttl="10m")
+df = conn.query('SELECT * FROM D_merged_processed limit 10;', ttl="Получившийся датафрейм из PostgreSQL БД")
 
 # for row in df.itertuples():
 #     st.write(f"{row.id} has a :{row.loan_id}:")
-    
+st.header('1. Просмотрим сам датафрейм')
+st.subheader('''В наличии должны быть колонки     - AGREEMENT_RK — уникальный идентификатор объекта в выборке;
+    - TARGET — целевая переменная: отклик на маркетинговую кампанию (1 — отклик был зарегистрирован, 0 — отклика не было);
+    - AGE — возраст клиента;
+    - SOCSTATUS_WORK_FL — социальный статус клиента относительно работы (1 — работает, 0 — не работает);
+    - SOCSTATUS_PENS_FL — социальный статус клиента относительно пенсии (1 — пенсионер, 0 — не пенсионер);
+    - GENDER — пол клиента (1 — мужчина, 0 — женщина);
+    - CHILD_TOTAL — количество детей клиента;
+    - DEPENDANTS — количество иждивенцев клиента;
+    - PERSONAL_INCOME — личный доход клиента (в рублях);
+    - LOAN_NUM_TOTAL — количество ссуд клиента;
+    - LOAN_NUM_CLOSED — количество погашенных ссуд клиента.''')
 st.dataframe(df, use_container_width=True)
