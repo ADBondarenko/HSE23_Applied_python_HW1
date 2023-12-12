3fec0d0f39da250e7e4a0372d900ff42678d383f
Artem Bondarenko <Regall.B@yandex.ru>
2023-12-12 18:32:13 +0300

    Added some charts

--- streamlit_app_EDA.py
+++ streamlit_app_EDA.py
@@ -5,6 +5,7 @@ import sqlalchemy
 from sqlalchemy import create_engine
 import psycopg2
 import os
+import matplotlib.pyplot as plt
 
 conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT * FROM D_merged_processed limit 10;', ttl="10m")
@@ -34,7 +35,22 @@ st.subheader('''В наличии должны быть колонки
 * вычисление числовых характеристик распределения числовых столбцов (среднее, min, max, медиана и так далее)''')
 engine_pd = create_engine(f"postgresql://{st.secrets.connections.postgresql.username}:{st.secrets.connections.postgresql.password}@{st.secrets.connections.postgresql.host}/{st.secrets.connections.postgresql.database}")
 df_full_pd = pd.read_sql('SELECT * FROM D_merged_processed;', engine_pd)
+st.subheader('''Построение графиков распределений признаков''')
 
+target_cols = ['age', 'gender', 'education', 'marital_status', 'child_total',
+       'dependants', 'socstatus_work_fl', 'socstatus_pens_fl',
+       'family_income', 'personal_income',
+      'target', 'loan_num_total', 'loan_num_closed']
+
+for tab_col, col in zip(st.tabs(target_cols),target_cols):
+    with tab_col:
+        tab_col.subheader(f"Распределение признака {col}")
+        
+        tab_col.bar_chart(df_full_pd[[target_col]])
+        
+        
+
+# for i in target_cols:
 # df_full = conn.query('SELECT * FROM D_merged_processed;', ttl="10m")
 # all_widgets = sp.create_widgets(df_full, create_data)
 # ignore_columns=["PassengerId"]
